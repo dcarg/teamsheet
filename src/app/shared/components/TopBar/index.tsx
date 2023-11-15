b95d302e2ef302a0fdbe21a9ef9b9577f309589d
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2023-11-16 08:54:52 +1100

    delete unused query

--- src/app/shared/components/TopBar/index.tsx
+++ src/app/shared/components/TopBar/index.tsx
@@ -1,10 +1,6 @@
-import { getSports } from '@queries/sport'
-
 import SelectTeam from './SelectTeam'
 
 const TopBar = async () => {
-  const sports = await getSports()
-
   return (
     <div className="border-b border-slate-500 flex flex-col items-center p-2 m-1 w-full">
       <div className="flex items-start max-w-screen-lg w-full">
--- src/app/shared/queries/sport.ts
+++
@@ -1,9 +0,0 @@
-import prisma from '@db/prismaSingleton'
-
-export const getSports = async () => {
-  const sports = await prisma.sport.findMany({
-    orderBy: { title: 'asc' },
-  })
-
-  return sports
-}
