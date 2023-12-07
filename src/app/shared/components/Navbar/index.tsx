828537fbb5387077ada06233ca11b4033c58e3ec
David Carr-Gregg <39669538+dcarg@users.noreply.github.com>
2023-12-07 22:06:37 +1100

    fix styling of Top Bar and NavBar to lineup, add Link around Navbar logo and Title

--- src/app/shared/components/Navbar/index.tsx
+++ src/app/shared/components/Navbar/index.tsx
@@ -1,22 +1,26 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const NavBar = async () => {
   return (
-    <div className="bg-cyan-700 flex flex h-[60px] p-2 w-full items-center">
-      {/* Insert TS Logo SVG HERE */} 
-      <div className="h-[40px] w-[40px] m-auto p-[5px]">
-        <Image
-          src="/ts-logo.svg"
-          alt="TeamSheet Logo"
-          priority
-          height={35}
-          width={35}
-        />
-      </div>
-      
-      <div className="flex items-center h-full max-w-column w-full text-white text-xl ml-2">
-        TEAMSHEET
-      </div>
+    <div className="bg-cyan-700 h-[60px] w-full ">
+      <Link href="/">
+        <div className="max-w-column flex m-auto p-2 w-full items-center">
+          <div className="h-[40px] w-[40px] m-auto p-[5px] pl-0">
+            <Image
+              src="/ts-logo.svg"
+              alt="TeamSheet Logo"
+              priority
+              height={35}
+              width={35}
+            />
+          </div>
+          
+          <div className="flex items-center h-full max-w-column w-full text-white text-xl ml-[5px]">
+            TEAMSHEET
+          </div>
+        </div>
+      </Link>
     </div>
   )
 }
