6d90a082ca88ef78579a92cbcc900fd8457d5f43
David Carr-Gregg <39669538+dcarg@users.noreply.github.com>
2023-12-07 21:47:11 +1100

    Add TS logo SVG to NavBar

--- src/app/shared/components/Navbar/index.tsx
+++ src/app/shared/components/Navbar/index.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image'
 
 const NavBar = async () => {
   return (
-    <div className="bg-black flex flex h-[60px] p-2 w-full items-center">
-      {/* Insert TS Logo SVG HERE */}
-      <div className="bg-cyan-700 h-[40px] w-[40px] text-center text-white text-xl m-auto">
-        TS
+    <div className="bg-cyan-700 flex flex h-[60px] p-2 w-full items-center">
+      {/* Insert TS Logo SVG HERE */} 
+      <div className="h-[40px] w-[40px] m-auto p-[5px]">
+        <Image
+          src="/ts-logo.svg"
+          alt="TeamSheet Logo"
+          priority
+          height={35}
+          width={35}
+        />
       </div>
       
       <div className="flex items-center h-full max-w-column w-full text-white text-xl ml-2">
