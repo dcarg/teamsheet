f394a0020468a24008349eca3f997bcc421479aa
David Carr-Gregg <39669538+dcarg@users.noreply.github.com>
2023-12-10 00:00:00 +1100

    remove defined heights

--- src/app/shared/components/Navbar/index.tsx
+++ src/app/shared/components/Navbar/index.tsx
@@ -9,10 +9,10 @@ const NavBar = async (props: NavbarProps) => {
   const { title } = props
 
   return (
-    <div className="bg-cyan-700 h-[60px] w-full ">
+    <div className="bg-cyan-700 w-full ">
       <div className="max-w-column flex m-auto p-2 w-full items-center">
         <Link href="/">
-          <div className="h-[40px] w-[40px] m-auto p-[5px] pl-0">
+          <div className="m-auto p-[5px] pl-0">
             <Image
               src="/ts-logo.svg"
               alt="TeamSheet Logo"
@@ -23,7 +23,7 @@ const NavBar = async (props: NavbarProps) => {
           </div>
         </Link>
         
-        <div className="flex items-center h-full max-w-column w-full text-white text-xl ml-[5px]">
+        <div className="flex align-middle items-center h-full max-w-column w-full text-white text-xl ml-[5px]">
           {title}
         </div>
       </div>
