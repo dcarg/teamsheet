c65b0e9808ab9467fef929ed337f0d9825323d1b
David Carr-Gregg <39669538+dcarg@users.noreply.github.com>
2023-12-09 23:47:05 +1100

    Just wrap logo in Link, update logo to 30x30

--- src/app/shared/components/Navbar/index.tsx
+++ src/app/shared/components/Navbar/index.tsx
@@ -10,23 +10,23 @@ const NavBar = async (props: NavbarProps) => {
 
   return (
     <div className="bg-cyan-700 h-[60px] w-full ">
-      <Link href="/">
-        <div className="max-w-column flex m-auto p-2 w-full items-center">
+      <div className="max-w-column flex m-auto p-2 w-full items-center">
+        <Link href="/">
           <div className="h-[40px] w-[40px] m-auto p-[5px] pl-0">
             <Image
               src="/ts-logo.svg"
               alt="TeamSheet Logo"
               priority
-              height={35}
-              width={35}
+              height={30}
+              width={30}
             />
           </div>
-          
-          <div className="flex items-center h-full max-w-column w-full text-white text-xl ml-[5px]">
-            {title}
-          </div>
+        </Link>
+        
+        <div className="flex items-center h-full max-w-column w-full text-white text-xl ml-[5px]">
+          {title}
         </div>
-      </Link>
+      </div>
     </div>
   )
 }
