69b1ce1852e1528341f58925950eb63e3b4dc6df
David Carr-Gregg <39669538+dcarg@users.noreply.github.com>
2023-12-10 13:06:17 +1100

    Update NameTeam Styling and Add IsEditing display for Title

--- src/app/shared/components/NameForm/index.tsx
+++ src/app/shared/components/NameForm/index.tsx
@@ -1,14 +1,33 @@
 'use client'
 
+import type { Dispatch, SetStateAction } from 'react'
 import { useState } from 'react'
 
 import clsx from 'clsx'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
 
 import type { TeamSheet } from '@prisma/client'
 
 import { updateTeamSheet } from '@actions/teamSheet'
 
 import Label from '@components/Label'
+
+type HandleEditTitleParams = {
+  callbacks: {
+    afterActionFn: Dispatch<SetStateAction<null>>,
+  },
+  id: string,
+  title: string,
+}
+
+const  handleEditTitle = async (params: HandleEditTitleParams) => {
+  const { id, title, callbacks: { afterActionFn } } = params
+
+  await updateTeamSheet({ id, title })
+  return afterActionFn()
+}
 
 type NameFormProps = {
   teamSheet: TeamSheet,
@@ -23,28 +42,51 @@ const NameForm = (props: NameFormProps) => {
   } = props
 
   const [title, setTitle] = useState(initTitle || '')
+  const [isEditing, setIsEditing] = useState(false)
 
   return (
-    <div className="py-3">
-      <Label>Team Name</Label>
+    <div className="p-2">
+      {!isEditing && (
+        <div className="flex justify-between items-center">
+          <div className="text-xl"> 
+            {title}
+          </div>
+          
+          <FontAwesomeIcon
+            icon={faEdit}
+            onClick={() => setIsEditing(true)}
+          />
+        </div>
+      )}
 
-      <div className="flex justify-between">
-        <input
-          className={clsx(
-            "border rounded p-2",
-            title ? "border-slate-500" : "border-rose-500"
-          )}
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        />
+      {isEditing && (
+        <>
+          <Label>Team Name</Label>
 
-        <button
-          className="border border-black p-1 rounded bg-green-400 hover:bg-green-500 text-slate-900 w-28"
-          onClick={() => updateTeamSheet({ id, title })}
-        >
-          Save
-        </button>
-      </div>
+          <div className="flex justify-between">
+            <input
+              className={clsx(
+                "border rounded p-2 w-full",
+                title ? "border-slate-500" : "border-rose-500"
+              )}
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+
+            <button
+              className="border border-black p-1 rounded bg-cyan-400 hover:bg-cyan-500 text-slate-900 w-28 ml-2"
+              onClick={() => handleEditTitle({ 
+                callbacks: { afterActionFn: setIsEditing(false) },
+                id,
+                title, 
+              })}
+            >
+              {title ? 'Update' : 'Save'}
+            </button>
+          </div>
+        </>
+      )}
+
 
     </div>
   )
