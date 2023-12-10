713003096e2e70a25734bf1a03cb53fcc1055900
David Carr-Gregg <39669538+dcarg@users.noreply.github.com>
2023-12-10 10:53:34 +1000

    TS-19 FEATURE - NavBar (#48)
    * Init Setup NavBar
    
    * Add local font futura and setup in next/tailwind
    
    * Add TS logo SVG to NavBar
    
    * Update Fill of logo to white
    
    * fix styling of Top Bar and NavBar to lineup, add Link around Navbar logo and Title
    
    * add title prop to Navbar component
    
    * Just wrap logo in Link, update logo to 30x30
    
    * move localFont variable to below imports
    
    * remove defined heights
    
    * removed unused styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,37 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import localFont from "next/font/local"
 import './globals.css'
 
 // Load fontawesome css at the root of the app to stop 'flashing' icon
-import '@fortawesome/fontawesome-svg-core/styles.css';
-import { config } from "@fortawesome/fontawesome-svg-core";
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from "@fortawesome/fontawesome-svg-core"
 config.autoAddCss = false;
 
+import NavBar from '@components/NavBar'
 import TopBar from '@components/TopBar'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const futura = localFont(
+  {
+    src: [
+      // {
+      //   path: "../../public/fonts/futura-pt.ttf",
+      //   weight: "400",
+      // },
+      {
+        path: "../../public/fonts/futura-pt-bold.ttf",
+        weight: "700",
+      },
+      // {
+      //   path: "../../public/fonts/futura-pt-light.ttf",
+      //   weight: "400",
+      // }
+    ],
+    variable: "--font-futura",
+  }
+)
 
 export const metadata: Metadata = {
   title: 'Teamsheet',
@@ -23,7 +45,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} flex flex-col items-center`}>
+      <body className={`${futura.className} ${inter.className} flex flex-col items-center`}>
+        <NavBar title="TEAMSHEET" />
+
         <TopBar />
 
         <div className="max-w-column w-full">
--- src/app/shared/components/TopBar/index.tsx
+++ src/app/shared/components/TopBar/index.tsx
@@ -2,8 +2,8 @@ import SelectTeam from './SelectTeam'
 
 const TopBar = async () => {
   return (
-    <div className="border-b border-slate-500 flex flex-col h-topBarHeight items-center p-2 mb-1 w-full">
-      <div className="flex items-start max-w-column w-full">
+    <div className="border-b border-slate-500 flex flex-col h-topBarHeight items-center mb-1 w-full">
+      <div className="flex items-start max-w-column w-full p-2">
         <SelectTeam />
       </div>
     </div>
