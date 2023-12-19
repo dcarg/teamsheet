d6f26fffe40bf8ecf612843be319f060483f6d61
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2023-12-19 15:37:54 +1100

    TS-30 FEATURE - Unselect a Selected Player (#70)
    * update removes player from teamsheet data
    
    * change colours of Unselect icon
    
    * fix nullable teamsheet warning
    
    * refactored UnSelectPlayer posiitoning to parent element
    
    * remove unused code

--- src/app/shared/components/Field/PlayerCard/index.tsx
+++ src/app/shared/components/Field/PlayerCard/index.tsx
@@ -7,6 +7,7 @@ import TeamContext from '@contexts/teamContext'
 import type { PlayerWithPositions } from '@types'
 
 import PlayerIcon from '@components/PlayerIcon'
+import UnSelectPlayer from '@components/UnSelectPlayer'
 
 interface PlayerCardProps {
   className: string,
@@ -29,11 +30,11 @@ const PlayerCard = (props: PlayerCardProps) => {
   } = teamContextValue
 
   const isPlayerSelected = !!player?.id
-  
+
   return (
-    <div className={`${className} h-[30px]`}> 
+    <div className={`${className} h-[30px]`}>
       <div
-        className="box-border border-black rounded cursor-pointer flex flex-col p-1 min-h-[50px] min-w-[80px] w-fit border hover:border-cyan-300"
+        className="box-border border-black rounded cursor-pointer flex flex-col p-1 min-h-[50px] min-w-[80px] w-fit border hover:border-cyan-300 relative"
         onClick={() => {
           setSelectedPosition(position)
           setSelectedTeamSheetLayoutId(teamSheetLayoutId)
@@ -46,6 +47,12 @@ const PlayerCard = (props: PlayerCardProps) => {
             unSelected={!isPlayerSelected}
             size='2x'
           />
+
+          {player && (
+            <div className="flex justify-center items-center border rounded-full h-5 w-5 absolute -top-2.5 -right-2.5 bg-slate-200">
+              <UnSelectPlayer teamSheetLayoutId={teamSheetLayoutId} />
+            </div>
+          )}
         </div>
 
         {player?.lastname || positionTitle}
