9e4a02f234c2a94837d33c080d70e301a03a9212
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2023-12-23 11:57:27 +1100

    refactored createUser to server action

--- src/app/shared/functions/user.ts
+++ src/app/shared/functions/user.ts
@@ -4,6 +4,8 @@ import type { KindeUser } from '@kinde-oss/kinde-auth-nextjs/dist/types'
 
 import type { User } from '@prisma/client'
 
+import { createUser } from '@actions/user'
+
 export const findOrCreateUser = async (externalUser: KindeUser | null) => {
   if (!externalUser) return null
 
@@ -21,19 +23,19 @@ export const findOrCreateUser = async (externalUser: KindeUser | null) => {
   })
 
   if (!user) {
-    user = await prisma.user.create({
-      data: {
-        email: externalUser.email,
-        firstname: externalUser.given_name,
-        lastname: externalUser.family_name,
-        userAccounts: {
-          create: {
-            provider: 'Kinde',
-            providerId: externalUser.id,
-          },
+    const payload = {
+      email: externalUser.email,
+      firstname: externalUser.given_name,
+      lastname: externalUser.family_name,
+      userAccounts: {
+        create: {
+          provider: 'Kinde',
+          providerId: externalUser.id,
         },
       },
-    })
+    }
+
+    user = await createUser(payload)
   }
 
   return user
