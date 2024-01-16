53032a6eb6fd609209e26f9598faaa1db885e846
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-01-17 07:46:18 +1100

    added text to OG image

--- src/app/api/ogImages/route.tsx
+++ src/app/api/ogImages/route.tsx
@@ -16,7 +16,56 @@ export const GET = async () => {
 
   return new ImageResponse(
     (
-      <div>og image here</div>
+      <div
+        style={{
+          alignItems: 'center',
+          background: 'radial-gradient(circle, rgba(38,38,43,1) 0%, rgba(40,40,46,1) 80%, rgba(0,99,35,1) 100%)',
+          display: 'flex',
+          flexDirection: 'column',
+          height: 630,
+          justifyContent: 'center',
+          width: 1200,
+        }}
+      >
+        <div
+          style={{
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+          }}
+        >
+          <div
+            style={{
+              color: 'white',
+              fontFamily: 'Futura Bold',
+              fontSize: 48,
+              fontWeight: 'bold',
+            }}
+          >
+            teamsheet.online
+          </div>
+
+          <img
+            src={`${process.env.NEXT_PUBLIC_VERCEL_URL}/ts_logo_white.svg`}
+            alt="TeamSheet Logo"
+            height={50}
+            width={50}
+            style={{ marginLeft: '32px' }}
+          />
+        </div>
+
+        <div
+          style={{
+            color: 'white',
+            fontFamily: 'Futura Bold',
+            fontSize: 48,
+            fontWeight: 'bold',
+            marginTop: 40,
+          }}
+        >
+          Who would you pick?
+        </div>
+      </div>
     ),
     {
       height: 630,
