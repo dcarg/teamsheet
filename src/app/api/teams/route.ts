a2cdfd59c14b5fe05360524932ac828b79792b0f
David Carr-Gregg <david.carr-gregg@domain.com.au>
2024-01-18 20:52:56 +1100

    add cascade delete to competitionTeams and remove sportKeyWhereClause for broken route temporarily

--- src/app/api/teams/route.ts
+++ src/app/api/teams/route.ts
@@ -5,12 +5,7 @@ import prisma from '@db/prismaSingleton'
 export const GET = async (request: NextRequest) => {
   const sportKey = request.nextUrl.searchParams.get('sportKey')
 
-  const sportKeyWhereClause = sportKey ? { sport: { key: sportKey } } : {}
-
   const teams = await prisma.team.findMany({
-    where: {
-      ...sportKeyWhereClause
-    },
     orderBy: {
       title: 'asc',
     },
