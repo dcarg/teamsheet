0596af77dcd2b7fe1885972ffcb31a77fafd39c4
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-01-27 10:20:16 +1100

    fix issue where url was not showing in tweet intent

--- src/app/shared/components/TweetButton/index.tsx
+++ src/app/shared/components/TweetButton/index.tsx
@@ -10,12 +10,13 @@ const TweetButton = (props: TweetButtonProps) =>  {
   const { shareUrl } = props
 
   const text = "This%20is%20who%20I%20would%20pick"
+  const url = encodeURIComponent(shareUrl)
 
   return (
     <div className="ml-2">
       <a
         className="twitter-share-button"
-        href={`https://twitter.com/intent/tweet?text=${text}&url=${shareUrl}`}
+        href={`https://twitter.com/intent/tweet?text=${text}&url=${url}`}
       />
     </div>
   )
