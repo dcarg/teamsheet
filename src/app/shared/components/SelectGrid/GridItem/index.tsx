4d87f42ced96c06303bb2e1ce270826e8a795b2d
David Carr-Gregg <david.carr-gregg@domain.com.au>
2024-03-06 23:50:22 +1100

    trying to add dynamic select grid background colors for teams wip

--- src/app/shared/components/SelectGrid/GridItem/index.tsx
+++ src/app/shared/components/SelectGrid/GridItem/index.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import clsx from 'clsx'
+
 import { usePathname } from 'next/navigation'
 
 import Image from "next/image"
@@ -7,21 +9,31 @@ import Link from "next/link"
 
 type GridItemProps = {
   entityLogo?: object,
-  entity: object,
+  entity: {
+    key?: string,
+    primaryColor?: string,
+    title: string,
+  },
   hideTitle?: boolean,
 }
 
 const GridItem = (props: GridItemProps) => {
   const { entityLogo, entity, hideTitle } = props || {}
   const { src } = entityLogo || {}
-  const { key, title } = entity || {}
+  const { key, primaryColor, title } = entity || {}
 
   const pathname = usePathname()
+
+  // the backgroundColor here on line 35 isn't working because tailwind doesn't handle dhynamic class names
+  // https://tailwindcss.com/docs/content-configuration#dynamic-class-names
 
   return (
     <Link href={`${pathname}/${key}`}>
       <div
-        className="flex flex-col h-[200px] w-[200px] justify-center items-center border rounded p-2"
+        className={clsx(
+          "flex flex-col h-[200px] w-[200px] justify-center items-center border rounded p-2",
+          primaryColor && `bg-[${primaryColor}]`
+        )}
         
       >
         <Image
