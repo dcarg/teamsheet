8f63c64de9abcaab8af3138b731c49e960a65026
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-03-23 10:51:39 +1100

    landing page WIP

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { redirect } from 'next/navigation'
+import Link from 'next/link'
 
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
@@ -10,5 +10,40 @@ export default async function Home() {
 
   findOrCreateUser(kindeUser)
 
-  redirect('/sport/rugby')
+  return (
+    <div className="flex-col items-center justify-center">
+      <div className="mt-[50px] text-2xl font-bold flex justify-center">
+        We've all got a team
+      </div>
+
+      <div className="mt-[50px] text-2xl font-bold flex justify-center">
+        And we all know better
+      </div>
+
+      <div className="text-2xl font-bold flex justify-center">
+        than the coaches...
+      </div>
+
+      <div className="mt-[50px] text-2xl font-bold flex justify-center">
+        Who would you pick?
+      </div>
+
+      <div className="mt-[50px] text-2xl font-bold flex justify-center">
+        Build your Teamsheet
+      </div>
+
+      <div className="text-2xl font-bold flex justify-center">
+        Go on
+      </div>
+
+      <div className="mt-[50px] flex justify-center">
+        <Link
+          className="inline p-2 border rounded bg-green-500 hover:bg-green-600 text-white font-semibold w-[150px] flex justify-center"
+          href="sport/rugby"
+        >
+          Start
+        </Link>
+      </div>
+    </div>
+  )
 }
