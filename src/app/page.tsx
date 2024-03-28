481a97e9f8d6c638f7948f0f7066f260ec78e7fd
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-03-29 09:15:57 +1100

    add text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,27 +14,27 @@ export default async function Home() {
   return (
     <div className="flex-col items-center justify-center">
       <div className="mt-[50px] text-2xl font-bold flex justify-center">
-        We've all got a team
+        WE'VE ALL GOT A TEAM
       </div>
 
       <div className="mt-[50px] text-2xl font-bold flex justify-center">
-        And we all know better
+        AND WE ALL KNOW BETTER
       </div>
 
       <div className="text-2xl font-bold flex justify-center">
-        than the coaches...
+        THAN THE COACHES...
       </div>
 
       <div className="mt-[50px] text-2xl font-bold flex justify-center">
-        Who would you pick?
+        WHO WOULD YOU PICK?
       </div>
 
       <div className="mt-[50px] text-2xl font-bold flex justify-center">
-        Build your Teamsheet
+        BUILD YOUR TEAMSHEET
       </div>
 
       <div className="text-2xl font-bold flex justify-center">
-        Go on
+        GO ON
       </div>
 
       <div className="mt-[50px] flex justify-center">
@@ -42,11 +42,47 @@ export default async function Home() {
           className="inline p-2 border rounded bg-green-500 hover:bg-green-600 text-white font-semibold w-[150px] flex justify-center"
           href="sport/rugby"
         >
-          Start
+          START
         </Link>
       </div>
 
-      <div className="h-[1227px] w-screen mt-[200px] bg-cover bg-[url('/landing-page-background.svg')]" />
+      <div className="h-[800px] w-screen mt-[200px] bg-cover bg-[url('/landing-page-background.svg')]">
+        <div className="mt-[50px] pt-[200px] text-2xl text-white font-bold flex justify-center">
+          TEAMSHEET IS SIMPLE
+        </div>
+
+        <div className="mt-[10px] text-xl text-white font-bold flex justify-center">
+          SELECT A SPORT
+        </div>
+
+        <div className="mt-[10px] text-xl text-white font-bold flex justify-center">
+          SELECT A COMPETITION
+        </div>
+
+        <div className="mt-[10px] text-xl text-white font-bold flex justify-center">
+          SELECT A TEAM
+        </div>
+
+        <div className="mt-[10px] text-xl text-white font-bold flex justify-center">
+          CREATE YOUR TEAMSHEET
+        </div>
+
+        <div className="mt-[10px] text-xl text-white font-bold flex justify-center">
+          SHARE IT WITH THE WORLD
+        </div>
+
+        <div className="mt-[50px] text-xl text-white font-bold flex justify-center">
+          POST TO SOCIALS
+        </div>
+
+        <div className="mt-[50px] text-xl text-white font-bold flex justify-center">
+          AND WE'RE ONLY JUST WARMING UP
+        </div>
+
+        <div className="mt-[10px] text-xl text-white font-bold flex justify-center">
+          MORE FEATURES TO COME...
+        </div>
+      </div>
     </div>
   )
 }
