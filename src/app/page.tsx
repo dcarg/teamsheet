09deb9d752f6646d3dcf70523cea6ab2d8b8d686
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-04-09 08:43:25 +1000

    fix text positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,29 +12,31 @@ export default async function Home() {
   findOrCreateUser(kindeUser)
 
   return (
-    <div className="flex-col items-center justify-center relative">
-      <div className="mt-[50px] text-2xl font-bold flex justify-center">
-        WE&apos;VE ALL GOT A TEAM
-      </div>
+    <div className="relative flex flex-col items-center">
+      <div className="w-[320px]">
+        <div className="mt-[50px] text-2xl font-bold">
+          WE&apos;VE ALL GOT A TEAM
+        </div>
 
-      <div className="mt-[50px] text-2xl font-bold flex justify-center">
-        AND WE ALL KNOW BETTER
-      </div>
+        <div className="mt-[50px] text-2xl font-bold">
+          AND WE ALL KNOW BETTER
+        </div>
 
-      <div className="text-2xl font-bold flex justify-center">
-        THAN THE COACHES...
-      </div>
+        <div className="text-2xl font-bold">
+          THAN THE COACHES...
+        </div>
 
-      <div className="mt-[50px] text-2xl font-bold flex justify-center">
-        WHO WOULD YOU PICK?
-      </div>
+        <div className="mt-[50px] text-2xl font-bold">
+          WHO WOULD YOU PICK?
+        </div>
 
-      <div className="mt-[50px] text-2xl font-bold flex justify-center">
-        BUILD YOUR TEAMSHEET
-      </div>
+        <div className="mt-[50px] text-2xl font-bold">
+          BUILD YOUR TEAMSHEET
+        </div>
 
-      <div className="text-2xl font-bold flex justify-center">
-        GO ON
+        <div className="text-2xl font-bold">
+          GO ON
+        </div>
       </div>
 
       <div className="mt-[50px] flex justify-center">
@@ -47,7 +49,7 @@ export default async function Home() {
       </div>
 
       <Image
-        className="absolute m-auto left-0 right-0 top-0 bottom-0 z-10"
+        className="mt-[50px] z-10"
         src="/landing_page_image.svg"
         alt="Photo of TeamSheet on mobile"
         priority
@@ -55,40 +57,40 @@ export default async function Home() {
         width={300}
       />
 
-      <div className="h-[900px] w-screen mt-[250px] bg-cover bg-[url('/landing-page-background.svg')] relative">
-        <div className=" pt-[280px]">
-          <div className="mt-[170px] text-2xl text-white font-bold flex justify-center">
-            TEAMSHEET IS SIMPLE
-          </div>
+      <div className="absolute top-[800px] min-h-[900px] w-screen bg-cover bg-[url('/landing-page-background.svg')]">
+        <div className="flex flex-col items-center">
+          <div className="mt-[220px] w-[320px]">
+            <div className="mt-[170px] text-2xl text-white font-bold">
+              TEAMSHEET IS SIMPLE
+            </div>
 
-          <div className="ml-[20%] mt-[20px]">
-            <div className="mt-[10px] text-xl text-white font-bold flex">
+            <div className="mt-[10px] text-xl text-white font-bold">
               SELECT A SPORT
             </div>
 
-            <div className="mt-[10px] text-xl text-white font-bold flex">
+            <div className="mt-[10px] text-xl text-white font-bold">
               SELECT A COMPETITION
             </div>
 
-            <div className="mt-[10px] text-xl text-white font-bold flex">
+            <div className="mt-[10px] text-xl text-white font-bold">
               SELECT A TEAM
             </div>
 
-            <div className="mt-[10px] text-xl text-white font-bold flex">
+            <div className="mt-[10px] text-xl text-white font-bold">
               CREATE YOUR TEAMSHEET
             </div>
 
-            <div className="mt-[10px] text-xl text-white font-bold flex">
+            <div className="mt-[10px] text-xl text-white font-bold">
               SHARE IT WITH THE WORLD
             </div>
-          </div>
 
-          <div className="mt-[70px] text-xl text-white font-bold flex justify-center">
-            AND WE&apos;RE ONLY JUST WARMING UP
-          </div>
+            <div className="mt-[70px] text-xl text-white font-bold">
+              AND WE&apos;RE ONLY JUST WARMING UP
+            </div>
 
-          <div className="mt-[10px] text-xl text-white font-bold flex justify-center">
-            MORE FEATURES TO COME...
+            <div className="mt-[10px] text-xl text-white font-bold">
+              MORE FEATURES TO COME...
+            </div>
           </div>
         </div>
       </div>
