c31d658a0cd3fdb7b699531498ad37b6d6d43777
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-04-11 08:17:44 +1000

    added link to teams on admin landing page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link"
+
 const Page = async () => {
   return (
-    <div>admin here</div>
+    <div className="flex flex-col">
+      <div className="font-bold mt-4 mx-4 text-center text-xl">Admin</div>
+
+      <Link
+        className="p-3 m-3 border rounded-lg hover:bg-gray-50"
+        href="/admin/teams"
+      >
+        Teams
+      </Link>
+    </div>
   )
 }
 
