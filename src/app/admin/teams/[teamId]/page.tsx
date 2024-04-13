842d7d34e43248330df8494e9cbe3b0fbf0381ee
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-04-13 21:11:10 +1000

    add tabs to team details page

--- src/app/admin/teams/[teamId]/page.tsx
+++ src/app/admin/teams/[teamId]/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import prisma from '@db/prismaSingleton'
 
 interface PageProps {
@@ -13,8 +15,38 @@ const Page = async (props: PageProps) => {
     where: { id: +teamId },
   })
 
+  const {
+    id,
+    key,
+    primaryColor,
+    secondaryColor,
+    title,
+  } = team
+
   return (
-    <div>team edit screen here</div>
+    <>
+      <div className="font-bold mt-4 mx-4 text-center text-xl">{title}</div>
+
+      <div className="flex flex-col">
+        <div className="flex">
+          <div className="border-b w-3" />
+
+          <Link className="bg-slate-100 border-l border-t p-2 rounded-t-xl" href={`/admin/teams/${id}`}>
+            Details
+          </Link>
+
+          <Link className="border p-2 rounded-t-xl" href={`/admin/teams/${id}/players`}>
+            Players
+          </Link>
+
+          <div className="grow border-b" />
+        </div>
+
+        <div className="bg-slate-100 p-3">
+          <div className="bg-white">Form here</div>  
+        </div>
+      </div>
+    </>
   )
 }
 
