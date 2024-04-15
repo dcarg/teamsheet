480a44b5c75d309e79ddd51bbc8d6e30716bb615
Mark Ball <54693327+Mark-Ball@users.noreply.github.com>
2024-04-16 08:14:03 +1000

    setup form fields

--- src/app/admin/teams/[teamId]/_TeamForm/index.tsx
+++ src/app/admin/teams/[teamId]/_TeamForm/index.tsx
@@ -19,7 +19,16 @@ import {
 import { Input } from '@components/shadcn/input'
 
 const formSchema = z.object({
-  title: z.string().min(1),
+  key: z.string().min(1, { message: 'Key is required' }),
+  primaryColor: z
+    .string()
+    .length(7, { message: 'Must be a hex code' })
+    .startsWith('#', { message: 'Must be a hex code' }),
+  secondaryColor: z
+    .string()
+    .length(7, { message: 'Must be a hex code' })
+    .startsWith('#', { message: 'Must be a hex code' }),
+  title: z.string().min(1, { message: 'Title is required' }),
 })
 
 const onSubmit = (values: z.infer<typeof formSchema>) => {
@@ -36,10 +45,16 @@ const TeamForm = (props: TeamFormProps) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
+      key: '',
+      primaryColor: '',
+      secondaryColor: '',
       title: '',
     },
   })
 
+  // TODO
+  // 2. team update server action
+  // 3. refactor form stuff to single hook if possible
 
   return (
     <Form {...form}>
@@ -49,21 +64,66 @@ const TeamForm = (props: TeamFormProps) => {
           name="title"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Title</FormLabel>
+              <FormLabel className="font-normal tracking-wider">Title</FormLabel>
 
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input {...field} />
               </FormControl>
-
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
 
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit">Submit</Button>
+
+        <FormField
+          control={form.control}
+          name="key"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-normal tracking-wider">Key</FormLabel>
+
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="primaryColor"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-normal tracking-wider">Primary Color</FormLabel>
+
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="secondaryColor"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-normal tracking-wider">Secondary Color</FormLabel>
+
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" variant="default">Submit</Button>
       </form>
     </Form>
   )
